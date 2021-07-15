7d08afbe17adb0e0ae521c9020cd812b2ed0f46e
Brandon <omstachu626@gmail.com>
2021-07-15 14:41:38 -0700

    user api signup route complete

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -29,6 +29,7 @@ router.post('/', asyncHandler(async (req, res, next) => {
   }),
 );
 
+// * sign out
 router.delete('/', (_req, res) => {
     res.clearCookie('token');
     return res.json ({message: 'success'})
