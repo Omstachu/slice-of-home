71b8d47e9044432bd222fa19528734c332ba94a9
Brandon <omstachu626@gmail.com>
2021-07-15 10:54:18 -0700

    finished error handling code in backend

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,8 @@ const helmet = require('helmet')
 const cookieParser = require('cookie-parser')
 const routes = require('./routes')
 
+const { ValidationError } = require('sequelize')
+
 const {environment} = require('./config')
 const isProduction = environment === 'production'
 
@@ -33,5 +35,36 @@ app.use(csurf({
 }))
 
 app.use(routes)
+
+// catch unhandled requests and forward to error handler
+app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = ["The requested resource couldn't be found."]
+    err.status = 404;
+    next(err)
+})
+
+// process sequelize errors
+app.use((err, _req, _res, next) => {
+    if (err instanceof ValidationError) {
+        err.errors = err.errors.map((e) => e.message);
+        err.title = 'Validation error'
+    }
+    next(err)
+})
+
+// error formatter
+app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+        title: err.title || 'Server Error',
+        message: err.message,
+        errors: err.errors,
+        stack: isProduction ? null : err.stack,
+    })
+})
+
 
 module.exports = app
