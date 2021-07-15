5b76f79ebf74abfa28b7242c89f829117efff74a
Brandon <omstachu626@gmail.com>
2021-07-15 15:02:44 -0700

    finished validation of signup request body

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,9 +4,11 @@ const apiRouter = require('./api')
 
 router.use('/api', apiRouter)
 
-router.get('/hello/world', function(req,res) {
-    res.cookie('XSRF-TOKEN', req.csrfToken())
-    res.send('Hello World')
-})
+
+// ? test routes
+// router.get('/hello/world', function(req,res) {
+//     res.cookie('XSRF-TOKEN', req.csrfToken())
+//     res.send('Hello World')
+// })
 
 module.exports = router;
