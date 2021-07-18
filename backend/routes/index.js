1622819f82871e9675aac1da67f5f69a0107887b
Brandon <omstachu626@gmail.com>
2021-07-17 21:04:12 -0700

    finished login component and signup thunk

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,13 +2,41 @@ const express = require('express')
 const router = express.Router()
 const apiRouter = require('./api')
 
+if (process.env.NODE_ENV !== 'production') {
+  router.get('/api/csrf/restore', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    return res.json({});
+  });
+}
+// Static routes
+// Serve React build files in production
+if (process.env.NODE_ENV === 'production') {
+    const path = require('path');
+    // Serve the frontend's index.html file at the root route
+    router.get('/', (req, res) => {
+      res.cookie('XSRF-TOKEN', req.csrfToken());
+      return res.sendFile(
+        path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+      );
+    });
+
+    // Serve the static assets in the frontend's build folder
+    router.use(express.static(path.resolve("../frontend/build")));
+
+    // Serve the frontend's index.html file at all other routes NOT starting with /api
+    router.get(/^(?!\/?api).*/, (req, res) => {
+      res.cookie('XSRF-TOKEN', req.csrfToken());
+      return res.sendFile(
+        path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+      );
+    });
+  }
+
+
+
 router.use('/api', apiRouter)
 
 
-// ? test routes
-// router.get('/hello/world', function(req,res) {
-//     res.cookie('XSRF-TOKEN', req.csrfToken())
-//     res.send('Hello World')
-// })
+
 
 module.exports = router;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,13 +6,19 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom'
 import App from './App';
+import { restoreCSRF, csrfFetch } from './store/csrf';
+import * as sessionActions from './store/session'
 
 import configureStore from './store'
 
 const store = configureStore()
 
-if(process.env.NODE_ENV !== 'production'){
+if (process.env.NODE_ENV !== 'production') {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
   window.store = store;
+  window.sessionActions = sessionActions
 }
 
 function Root(){
